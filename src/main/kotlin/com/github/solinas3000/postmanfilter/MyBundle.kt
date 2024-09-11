645e690ef26bf6fed059bb32c867db306e567c21
GitHub Action <action@github.com>
2024-09-11 14:08:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/solinas3000/postmanfilter/MyBundle.kt
+++ src/main/kotlin/com/github/solinas3000/postmanfilter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.solinas3000.postmanfilter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/solinas3000/postmanfilter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/solinas3000/postmanfilter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.solinas3000.postmanfilter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
